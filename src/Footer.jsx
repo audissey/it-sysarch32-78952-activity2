4fc26737809fea560490f2d8b4b845827e1f9696
Audissey Bardelas <78952-22265615@SERVER2.DOMAIN>
2024-03-02 18:53:03 +0800

    audissey

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} 2024 IT-SYSARCH32-78952 Made by Audissey Bardelas</p>
+        </footer>
+    );
+
+}
+
+export default Footer
